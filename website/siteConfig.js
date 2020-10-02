e2ffcf3afc7c446c0e2345e355ab88b70972088b
Aline <66221314+aoliveiramagalhaes@users.noreply.github.com>
2020-10-02 16:14:35 -0400

    first commit - update profile

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,18 +12,18 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Aline Oliveira Magalhaes', // Title for your website.
+    tagline: 'Continuously improving, aspiring front end developer.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
-    baseUrl: '/tech-portfolio/', // Base URL for your project */
+    url: 'https://aoliveiramagalhaes.github.io/portfolio', // Your website URL
+    baseUrl: 'aoliveiramagalhaes/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'Aline - Grace Hopper Celebration',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
-        twitter: '',
-        github: '',
+        linkedin: 'aline-magalhaes',
+        twitter: 'aomagalhaes',
+        github: 'aoliveiramagalhaes',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
